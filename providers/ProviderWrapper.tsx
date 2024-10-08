5747ea3da251462481ba4538302424837b270be1
Stoldt <lstold22@student.aau.dk>
2024-10-08 13:59:45 +0200

    Revert "Added SelectField for Activities in CopyModal"
    This reverts commit 773dabef7c8017c155efab440a6504cdb22a4307.

--- providers/ProviderWrapper.tsx
+++ providers/ProviderWrapper.tsx
@@ -1,99 +1,47 @@
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import DateProvider from './DateProvider';
-import {dateToQueryKey} from '../hooks/useActivity';
-import {TaskDTO} from '../types/TaskDTO';
+import { dateToQueryKey } from '../hooks/useActivity';
+import { TaskDTO } from '../types/TaskDTO';
 
 type CustomLayoutProps = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 const queryClient = new QueryClient();
 
-const ProviderWrapper = ({children}: CustomLayoutProps) => {
+const ProviderWrapper = ({ children }: CustomLayoutProps) => {
+  const mockData: TaskDTO[] = [
+    {
+      id: 1,
+      name: 'Mock Task 1',
+      startTime: new Date(),
+      endTime: new Date(),
+    },
+    {
+      id: 2,
+      name: 'Mock Task 2',
+      startTime: new Date(),
+      endTime: new Date(),
+    },
+  ];
 
-    const mockDataToday: TaskDTO[] = [
-        {
-            id: 0,
-            name: "Today 1",
-            startTime: (() => {
-                const date = new Date();
-                date.setHours(6, 0, 0, 0); // Set to 6:00 AM
-                return date;
-            })(),
-            endTime: (() => {
-                const date = new Date();
-                date.setHours(7, 0, 0, 0); // Set to 7:00 AM
-                return date;
-            })(),
-        },
-        {
-            id: 1,
-            name: "Today 2",
-            startTime: (() => {
-                const date = new Date();
-                date.setHours(7, 0, 0, 0); // Set to 7:00 AM
-                return date;
-            })(),
-            endTime: (() => {
-                const date = new Date();
-                date.setHours(8, 0, 0, 0); // Set to 8:00 AM
-                return date;
-            })(),
-        },
-    ];
+  const sourceDate = new Date();
+  const key = dateToQueryKey(sourceDate);
+  const key2 = dateToQueryKey(
+    new Date(new Date().setDate(new Date().getDate() + 1))
+  );
 
-    const mockDataTomorrow: TaskDTO[] = [
-        {
-            id: 0,
-            name: "Tomorrow 1",
-            startTime: (() => {
-                const date = new Date();
-                date.setDate(date.getDate() + 1);
-                date.setHours(6, 0, 0, 0); // Set to 6:00 AM
-                return date;
-            })(),
-            endTime: (() => {
-                const date = new Date();
-                date.setDate(date.getDate() + 1);
-                date.setHours(7, 0, 0, 0); // Set to 7:00 AM
-                return date;
-            })(),
-        },
-        {
-            id: 1,
-            name: "Tomorrow 2",
-            startTime: (() => {
-                const date = new Date();
-                date.setDate(date.getDate() + 1);
-                date.setHours(7, 0, 0, 0); // Set to 7:00 AM
-                return date;
-            })(),
-            endTime: (() => {
-                const date = new Date();
-                date.setDate(date.getDate() + 1);
-                date.setHours(8, 0, 0, 0); // Set to 8:00 AM
-                return date;
-            })(),
-        },
-    ];
+  queryClient.setQueryData(key, mockData);
+  queryClient.setQueryData(key2, mockData);
 
-    const sourceDate = new Date();
-    const key = dateToQueryKey(sourceDate);
-    const key2 = dateToQueryKey(
-        new Date(new Date().setDate(new Date().getDate() + 1))
-    );
-
-    queryClient.setQueryData(key, mockDataToday);
-    queryClient.setQueryData(key2, mockDataTomorrow);
-
-    return (
-        <QueryClientProvider client={queryClient}>
-            <GestureHandlerRootView>
-                <DateProvider>{children}</DateProvider>
-            </GestureHandlerRootView>
-        </QueryClientProvider>
-    );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GestureHandlerRootView>
+        <DateProvider>{children}</DateProvider>
+      </GestureHandlerRootView>
+    </QueryClientProvider>
+  );
 };
 
 export default ProviderWrapper;
