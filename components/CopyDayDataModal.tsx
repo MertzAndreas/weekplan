567c44deb7bc6464d3f051ff09fadf097299f3e4
Stoldt <lstold22@student.aau.dk>
2024-10-08 14:04:22 +0200

    Frick go back
    Put changes into the version from earlier

--- components/CopyDayDataModal.tsx
+++ components/CopyDayDataModal.tsx
@@ -5,10 +5,12 @@ import {
   View,
   StyleSheet,
   TouchableOpacity,
-} from 'react-native';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import useCopyDataModal from '../hooks/useCopyDataModal';
-import { useState } from 'react';
+  ScrollView,
+} from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import useCopyDataModal from "../hooks/useCopyDataModal";
+import React, { useState, useEffect } from "react";
+import { TaskDTO } from "../types/TaskDTO";
 
 type CopyDayDataModalProps = {
   destinationDate?: Date;
@@ -34,22 +36,25 @@ export const CopyDayDataModal = ({
       transparent={true}
       onRequestClose={() => {
         setModalVisible(false);
-      }}>
+      }}
+    >
       <TouchableOpacity
         style={styles.modalBackground}
         onPress={() => {
           setModalVisible(false);
-        }}>
+        }}
+      >
         <View
           style={styles.modalContainer}
-          onStartShouldSetResponder={() => true}>
+          onStartShouldSetResponder={() => true}
+        >
           <Text style={styles.header}>Kopier Aktiviteter</Text>
           <DateTimePicker
             value={dates.sourceDate ?? new Date()}
             is24Hour={true}
             mode="date"
             onChange={(_event, date) => {
-              handleDateChange(date, 'source');
+              handleDateChange(date, "source");
             }}
           />
           {error && <Text>{error}</Text>}
@@ -64,7 +69,7 @@ export const CopyDayDataModal = ({
             is24Hour={true}
             mode="date"
             onChange={(_event, date) => {
-              handleDateChange(date, 'destination');
+              handleDateChange(date, "destination");
             }}
           />
           <Button
@@ -80,27 +85,106 @@ export const CopyDayDataModal = ({
   );
 };
 
+const ActivityList = (activities: TaskDTO[]) => {
+  const [selectedActivities, setSelectedActivities] = useState<boolean[]>(
+    new Array(activities.length).fill(true),
+  );
+
+  function toggleSelected(index: number) {
+    const updatedActivities = [...selectedActivities];
+    updatedActivities[index] = !updatedActivities[index];
+    setSelectedActivities(updatedActivities);
+  }
+
+  function findSelected(): TaskDTO[] {
+    return activities.filter((_activity, index) => selectedActivities[index]);
+  }
+
+  useEffect(() => {
+    setSelectedActivities(new Array(activities.length).fill(true));
+    console.log("Updated activities");
+  }, [activities]);
+
+  return (
+    <ScrollView style={styles.activityView}>
+      {activities.map((activity, index) => {
+        const activityName = activity.name
+          ? activity.name.length <= 27
+            ? activity.name
+            : activity.name.substring(0, 25).concat("...")
+          : "";
+        const startTime = activity.startTime.toLocaleTimeString("da-DK", {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+        const endTime = activity.endTime.toLocaleTimeString("da-DK", {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+
+        return (
+          <TouchableOpacity onPress={() => toggleSelected(index)}>
+            <View
+              style={[
+                styles.activityEntry,
+                { borderLeftWidth: selectedActivities[index] ? 1 : 0 },
+              ]}
+              key={index.toString()}
+            >
+              <Text style={{ width: "70%" }}>{activityName}</Text>
+              <Text style={{ width: "30%", textAlign: "center" }}>
+                {startTime + "\n" + endTime}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        );
+      })}
+    </ScrollView>
+  );
+};
+
 const styles = StyleSheet.create({
+  activityView: {
+    padding: 5,
+    width: "100%",
+    height: "100%",
+    borderRadius: 15,
+    // borderStyle: "solid",
+    // borderWidth: 1,
+    // borderColor: "gray",
+  },
+  activityEntry: {
+    display: "flex",
+    flexDirection: "row",
+    marginBottom: 2,
+    alignItems: "center",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 1,
+    borderColor: "blue",
+    paddingLeft: 5,
+  },
   modalBackground: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
   modalContainer: {
-    width: '80%',
+    width: "80%",
+    height: "80%",
     padding: 30,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    display: 'flex',
-    gap: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+    gap: 10,
   },
   header: {
     fontSize: 16,
-    fontWeight: '600',
+    fontWeight: "600",
     marginBottom: 10,
-    color: '#333',
+    color: "#333",
   },
 });
--- providers/ProviderWrapper.tsx
+++ providers/ProviderWrapper.tsx
@@ -1,8 +1,8 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import DateProvider from './DateProvider';
-import { dateToQueryKey } from '../hooks/useActivity';
-import { TaskDTO } from '../types/TaskDTO';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import DateProvider from "./DateProvider";
+import { dateToQueryKey } from "../hooks/useActivity";
+import { TaskDTO } from "../types/TaskDTO";
 
 type CustomLayoutProps = {
   children: React.ReactNode;
@@ -14,13 +14,13 @@ const ProviderWrapper = ({ children }: CustomLayoutProps) => {
   const mockData: TaskDTO[] = [
     {
       id: 1,
-      name: 'Mock Task 1',
+      name: "Mock Task 1",
       startTime: new Date(),
       endTime: new Date(),
     },
     {
       id: 2,
-      name: 'Mock Task 2',
+      name: "Mock Task 2",
       startTime: new Date(),
       endTime: new Date(),
     },
@@ -29,7 +29,7 @@ const ProviderWrapper = ({ children }: CustomLayoutProps) => {
   const sourceDate = new Date();
   const key = dateToQueryKey(sourceDate);
   const key2 = dateToQueryKey(
-    new Date(new Date().setDate(new Date().getDate() + 1))
+    new Date(new Date().setDate(new Date().getDate() + 1)),
   );
 
   queryClient.setQueryData(key, mockData);
